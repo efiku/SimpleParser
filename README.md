c86014f07a57e571cbda9c9574e3c6f614541ed6
Krzysztof Pazdur <pazdurk@gmail.com>
2015-12-30 23:43:46 +0100

    Added build status and removed docs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Śmieszna mała aplikacja :)
+
+[![Build Status](https://travis-ci.com/efiku/SimpleParser.svg?token=xCpwqAxDjMzyxjCPykfo&branch=master)](https://travis-ci.com/efiku/SimpleParser)
+
+License: MIT
--- src/SimpleParser.php
+++ src/SimpleParser.php
@@ -25,12 +25,6 @@ class SimpleParser implements \Iterator
         $this->filePointer = fopen($fileName, "r");
     }
 
-    /**
-     * Return the current element
-     * @link http://php.net/manual/en/iterator.current.php
-     * @return mixed Can return any type.
-     * @since 5.0.0
-     */
     public function current(): string
     {
         $this->currentElement = trim(fgets($this->filePointer));
@@ -38,31 +32,16 @@ class SimpleParser implements \Iterator
         return $this->currentElement;
     }
 
-    /**
-     * Move forward to next element
-     * @since 5.0.0
-     */
     public function next()
     {
         ++$this->rowCounter;
     }
 
-    /**
-     * Return the key of the current element
-     * @link http://php.net/manual/en/iterator.key.php
-     * @return mixed scalar on success, or null on failure.
-     * @since 5.0.0
-     */
     public function key() : int
     {
         return $this->rowCounter;
     }
 
-    /**
-     * Checks if current position is valid
-     * @link http://php.net/manual/en/iterator.valid.php
-     * @return false|true
-     */
     public function valid() : bool
     {
         if (feof($this->filePointer)) {
@@ -72,11 +51,6 @@ class SimpleParser implements \Iterator
         return true;
     }
 
-    /**
-     * Rewind the Iterator to the first element
-     * @link http://php.net/manual/en/iterator.rewind.php
-     * @since 5.0.0
-     */
     public function rewind()
     {
         $this->rowCounter = 0;
